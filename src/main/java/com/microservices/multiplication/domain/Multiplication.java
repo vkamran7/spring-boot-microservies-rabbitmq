a88ef9026d73b31bb73ce690f945e77fafabf3bc
vkamran7 <vkamran7@gmail.com>
2021-12-21 23:44:18 +0400

    adding Multiplication class

---
+++ src/main/java/com/microservices/multiplication/domain/Multiplication.java
@@ -0,0 +1,39 @@
+package com.microservices.multiplication.domain;
+
+/**
+ * This class represents Multiplication
+ */
+public class Multiplication {
+
+    private int factorA;
+    private int factorB;
+    private int result;
+
+    public Multiplication(int factorA, int factorB) {
+        this.factorA = factorA;
+        this.factorB = factorB;
+        this.result = factorA * factorB;
+    }
+
+    public int getFactorA() {
+        return factorA;
+    }
+
+    public int getFactorB() {
+        return factorB;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Multiplication{" +
+                "factorA=" + factorA +
+                ", factorB=" + factorB +
+                ", result(A*B)=" + result +
+                '}';
+    }
+
+}
