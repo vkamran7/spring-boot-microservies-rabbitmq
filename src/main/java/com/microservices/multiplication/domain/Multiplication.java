3ad65d5d411755eb69a1881cb9ded5739f35674d
vkamran7 <vkamran7@gmail.com>
2021-12-22 21:14:20 +0400

    modifying Multiplication class

--- src/main/java/com/microservices/multiplication/domain/Multiplication.java
+++ src/main/java/com/microservices/multiplication/domain/Multiplication.java
@@ -1,39 +1,26 @@
 package com.microservices.multiplication.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
 /**
  * This class represents Multiplication
  */
-public class Multiplication {
+@RequiredArgsConstructor
+@Getter
+@ToString
+@EqualsAndHashCode
+public final class Multiplication {
 
-    private int factorA;
-    private int factorB;
+    private final int factorA;
+    private final int factorB;
     private int result;
 
-    public Multiplication(int factorA, int factorB) {
-        this.factorA = factorA;
-        this.factorB = factorB;
-        this.result = factorA * factorB;
-    }
-
-    public int getFactorA() {
-        return factorA;
-    }
-
-    public int getFactorB() {
-        return factorB;
-    }
-
-    public int getResult() {
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Multiplication{" +
-                "factorA=" + factorA +
-                ", factorB=" + factorB +
-                ", result(A*B)=" + result +
-                '}';
+    // Empty constructor for JSON (de)serialization
+    Multiplication() {
+        this(0,0);
     }
 
 }
