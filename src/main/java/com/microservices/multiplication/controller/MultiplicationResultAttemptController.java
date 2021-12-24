45f8f0acb88bd67dab204dc56090272a22e2ab2b
vkamran7 <vkamran7@gmail.com>
2021-12-24 16:26:28 +0400

    adding MultiplicationResultAttemptController class

---
+++ src/main/java/com/microservices/multiplication/controller/MultiplicationResultAttemptController.java
@@ -0,0 +1,26 @@
+package com.microservices.multiplication.controller;
+
+import com.microservices.multiplication.service.MultiplicationService;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/results")
+public final class MultiplicationResultAttemptController {
+
+    private final MultiplicationService multiplicationService;
+
+    public MultiplicationResultAttemptController(MultiplicationService multiplicationService) {
+        this.multiplicationService = multiplicationService;
+    }
+
+    @RequiredArgsConstructor
+    @NoArgsConstructor(force = true)
+    @Getter
+    private static final class ResultResponse {
+        private final boolean correct;
+    }
+}
