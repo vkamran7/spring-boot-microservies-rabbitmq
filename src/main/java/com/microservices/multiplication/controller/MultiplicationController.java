e467029527ed279d22b84a5508dfe7f86ea58e3b
vkamran7 <vkamran7@gmail.com>
2021-12-24 15:46:07 +0400

    modifying MultiplicationController class

--- src/main/java/com/microservices/multiplication/controller/MultiplicationController.java
+++ src/main/java/com/microservices/multiplication/controller/MultiplicationController.java
@@ -1,14 +1,23 @@
 package com.microservices.multiplication.controller;
 
+import com.microservices.multiplication.domain.Multiplication;
 import com.microservices.multiplication.service.MultiplicationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MultiplicationController {
+@RequestMapping("/multiplications")
+public final class MultiplicationController {
 
     private final MultiplicationService multiplicationService;
 
     MultiplicationController(final MultiplicationService multiplicationService) {
         this.multiplicationService = multiplicationService;
+    }
+
+    @GetMapping("/random")
+    Multiplication getMultiplication() {
+        return multiplicationService.createRandomMultiplication();
     }
 }
