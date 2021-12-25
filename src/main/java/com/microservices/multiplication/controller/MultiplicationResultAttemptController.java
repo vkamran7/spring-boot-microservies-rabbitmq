f63e563baf8ea6a5e728e064eccf113d634a8aab
vkamran7 <vkamran7@gmail.com>
2021-12-25 11:57:08 +0400

    adding postResult method to controller

--- src/main/java/com/microservices/multiplication/controller/MultiplicationResultAttemptController.java
+++ src/main/java/com/microservices/multiplication/controller/MultiplicationResultAttemptController.java
@@ -1,9 +1,13 @@
 package com.microservices.multiplication.controller;
 
+import com.microservices.multiplication.domain.MultiplicationResultAttempt;
 import com.microservices.multiplication.service.MultiplicationService;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +19,11 @@ public final class MultiplicationResultAttemptController {
 
     public MultiplicationResultAttemptController(MultiplicationService multiplicationService) {
         this.multiplicationService = multiplicationService;
+    }
+
+    @PostMapping
+    ResponseEntity<ResultResponse> postResult(@RequestBody MultiplicationResultAttempt attempt) {
+        return ResponseEntity.ok(new ResultResponse(multiplicationService.checkAttempt(attempt)));
     }
 
     @RequiredArgsConstructor
