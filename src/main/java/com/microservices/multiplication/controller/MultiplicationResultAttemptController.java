26e7e79d8e10c3116e4038bc11fcd8e7ff1fd1f8
vkamran7 <vkamran7@gmail.com>
2022-01-05 12:09:41 +0400

    adding method to MultiplicationResultAttemptController for user statistics

--- src/main/java/com/microservices/multiplication/controller/MultiplicationResultAttemptController.java
+++ src/main/java/com/microservices/multiplication/controller/MultiplicationResultAttemptController.java
@@ -3,10 +3,9 @@ package com.microservices.multiplication.controller;
 import com.microservices.multiplication.domain.MultiplicationResultAttempt;
 import com.microservices.multiplication.service.MultiplicationService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/results")
@@ -29,5 +28,10 @@ public final class MultiplicationResultAttemptController {
         );
 
         return ResponseEntity.ok(attemptCopy);
+    }
+
+    @GetMapping
+    ResponseEntity<List<MultiplicationResultAttempt>> getStatistics(@RequestParam("alias") String alias) {
+        return ResponseEntity.ok(multiplicationService.getStatsForUser(alias));
     }
 }
