fe47e0a8656e63c9c56f51325de5466e41820247
vkamran7 <vkamran7@gmail.com>
2022-01-05 12:07:28 +0400

    adding test for user stats

--- src/test/java/com/microservices/multiplication/service/MultiplicationServiceImplTest.java
+++ src/test/java/com/microservices/multiplication/service/MultiplicationServiceImplTest.java
@@ -6,12 +6,14 @@ import com.microservices.multiplication.domain.User;
 import com.microservices.multiplication.repository.MultiplicationRepository;
 import com.microservices.multiplication.repository.MultiplicationResultAttemptRepository;
 import com.microservices.multiplication.repository.UserRepository;
+import org.assertj.core.util.Lists;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -80,5 +82,23 @@ public class MultiplicationServiceImplTest {
         boolean attemptResult = multiplicationService.checkAttempt(attempt);
         assertThat(attemptResult).isFalse();
         verify(attemptRepository).save(attempt);
+    }
+
+    @Test
+    public void retrieveStatsTest() {
+        Multiplication multiplication = new Multiplication(50, 60);
+        User user = new User("Kamran");
+        MultiplicationResultAttempt attempt1 = new MultiplicationResultAttempt(
+                user, multiplication, 3010, false
+        );
+        MultiplicationResultAttempt attempt2 = new MultiplicationResultAttempt(
+                user, multiplication, 3051, false
+        );
+        List<MultiplicationResultAttempt> latestAttempts = Lists.newArrayList(attempt1, attempt2);
+        given(userRepository.findByAlias("Kamran")).willReturn(Optional.empty());
+        given(attemptRepository.findTop5ByUserAliasOrderByIdDesc("Kamran")).willReturn(latestAttempts);
+
+        List<MultiplicationResultAttempt> latestAttemptResult = multiplicationService.getStatsForUser("Kamran");
+        assertThat(latestAttemptResult).isEqualTo(latestAttempts);
     }
 }
